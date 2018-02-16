946cbad04cc0f07e1bbee07640be08c736c96e08
myself <myself@gmail.com>
2018-02-16 10:21:29 +0800

    almost finish study on marshmallow

--- README.md
+++ README.md
@@ -14,3 +14,33 @@ flask-restful provide a easy way to build restful api for us
 #### filtering output
 `Schema(only=("name","age"))`, when we pass `only` args, we can specify which filed we want to output
 > you can also use `exclude` to exclude fileds
+
+> note: set it this format 'only/exclude=("filed",)' when only filter one filed
+
+#### set the result ordered
+``` python
+# create schema's meta class and set order=true
+class SomeSchema(Schema):
+    ...
+    
+    class Meta():
+        ordered = True
+```
+
+### test
+``` bash
+# GET
+http://127.0.0.1/res
+
+# POST
+{
+    "name": "Bob2",
+    "phone": 93578747,
+    "email": "bobtest2@email.com",
+    "data": {
+        "school": "some school2",
+        "class": "some class2"
+    }
+}
+http://127.0.0.1/res
+```
--- src/app.py
+++ src/app.py
@@ -7,43 +7,79 @@ from marshmallow import Schema, fields, post_load
 app = Flask(__name__)
 api = Api(app)
 
+
+##########################################################################
+# object will be serialized from and deserialize to
+##########################################################################
 class ResObj(object):
-    def __init__(self, name, created_at, phone=None, email=None, data=None):
+    """
+    ResObj is resource object
+    which stores resource information and data object which is ResDataObj instance
+    """
+    def __init__(self, name, phone=None, email=None, data=None):
         self.name = name
         self.phone = phone
         self.email = email
         self.data = data
-        self.created_at = created_at
+        self.created_at = self.check_created_at_exist_or_not()
         self.updated_at = datetime.datetime.now()
 
     def __repr__(self):
         return '<User(name={self.name!r})>'.format(self=self)
     
-    @classmethod
-    def make_resobj(cls, *args, **kwargs):
-        created_at = datetime.datetime.now
-        args = list(args)
-        args[:-1].append(created_at)
-        print (args)
-        return cls(*args, **kwargs)
+    def check_created_at_exist_or_not(self):
+        # here we can do some real check
+        # if object already exist, return the original created_at
+        # if not, return the time now
+        return datetime.datetime.now()
+
+class ResDataObj(object):
+    """
+    ResDataObj is resource data object
+    which stores resource data belongs to ResObj
+    """
+    def __init__(self, school, class_num):
+        self.school = school
+        self.class_num = class_num
+
+
+##########################################################################
+# schema is the pipe function to serialize and deserialize
+##########################################################################
+class ResDataSchema(Schema):
+    school = fields.Str()
+    class_num = fields.Str(dump_to="class", load_from="class")
+
+    class Meta():
+        ordered = True
+
 
 
 class ResSchema(Schema):
+    # when set "required=True", An error will be stored if the the value is
+    # missing from the input to Schema.load().
     name = fields.Str(
         required=True,
         error_messages={'required': {'message': 'name required', 'code': 400}}
         )
     phone = fields.Integer()
     email = fields.Email()
-    created_at = fields.DateTime()
-    updated_at = fields.DateTime()
-    data = fields.Dict()
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    data = fields.Nested(ResDataSchema)
+
+    class Meta():
+        ordered = True
 
     @post_load
-    def make_res(self, data):
+    def new(self, data):
         return ResObj(**data)
 
 
+##########################################################################
+# Resource is flask-restful's class which can provide different
+# function fitting to specified http method
+##########################################################################
 class Res(Resource):
     def __init__(self):
         super(Res, self).__init__()
@@ -54,7 +90,8 @@ class Res(Resource):
         return result
     
     def post(self):
-        schema = ResSchema(exclude=("created_at"))
+        # schema = ResSchema(exclude=("created_at",))
+        schema = ResSchema()
         result = schema.load(request.json)
         if result.errors:
             result = result.errors
@@ -66,14 +103,16 @@ api.add_resource(Res, '/res')
 
 
 if __name__ == '__main__':
-    res_obj = ResObj.make_resobj(
+    # prepare data to be dump
+    res_obj = ResObj(
             "Bob",
-            datetime.datetime.now(),
             phone="93578746",
             email="bobtest@email.com",
             data={
                 "school": "some school",
-                "class": "some class"
+                "class_num": "some class1"
                 }
         )
+    
+    # run server
     app.run(host="0.0.0.0", port=80, debug=True)
